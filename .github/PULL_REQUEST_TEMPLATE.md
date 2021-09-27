1935c8b6872833c61eecd5badd79f1e2cc2461cf
Allison Ryan <allisonryan0002@gmail.com>
2021-09-27 14:51:27 -0500

    feat: replace default app with VGC app

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -10,21 +10,14 @@
 
 <!--- Describe your changes in detail -->
 
-## Type of Change:
+## Type of Change
 
 <!--- Put an `x` in all the boxes that apply: -->
 
-- [ ] New feature (non-breaking change which adds functionality)
-- [ ] Bug fix (non-breaking change which fixes an issue)
-- [ ] Breaking change (fix or feature that would cause existing functionality to change)
-- [ ] Code refactor
-- [ ] Build configuration change
-
-## Checklist
-
-<!--- Put an `x` in all the boxes that apply. -->
-
-- [ ] I added tests to cover my changes
-- [ ] I merged the target branch so this branch can be merged easily
-- [ ] I have updated the documentation accordingly (if applicable)
-- [ ] I ran the app on iOS to check for Podfile updates
+- [ ] ✨ New feature (non-breaking change which adds functionality)
+- [ ] 🛠️ Bug fix (non-breaking change which fixes an issue)
+- [ ] ❌ Breaking change (fix or feature that would cause existing functionality to change)
+- [ ] 🧹 Code refactor
+- [ ] ✅ Build configuration change
+- [ ] 📝 Documentation
+- [ ] 🗑️ Chore
---
+++ lib/bootstrap.dart
@@ -0,0 +1,38 @@
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/widgets.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log('onChange(${bloc.runtimeType}, $change)');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    super.onError(bloc, error, stackTrace);
+  }
+}
+
+Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  Bloc.observer = AppBlocObserver();
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+
+  await runZonedGuarded(
+    () async => runApp(await builder()),
+    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+  );
+}
