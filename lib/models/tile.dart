df17aac82175be1356a10233b6dd821939886236
Allison Ryan <allisonryan0002@gmail.com>
2021-09-29 14:59:37 -0500

    refactor: revert nullable tile position change

--- lib/models/tile.dart
+++ lib/models/tile.dart
@@ -6,14 +6,14 @@ import 'package:very_good_slide_puzzle/models/models.dart';
 /// {@endtemplate}
 class Tile extends Equatable {
   /// {@macro tile}
-  const Tile({required this.correctPosition, this.currentPosition});
+  const Tile({required this.correctPosition, required this.currentPosition});
 
   /// The correct 2D [Position] of the [Tile]. All tiles must be in their
   /// correct position to complete the puzzle.
   final Position correctPosition;
 
   /// The current 2D [Position] of the [Tile].
-  final Position? currentPosition;
+  final Position currentPosition;
 
   @override
   List<Object> get props => [correctPosition];
