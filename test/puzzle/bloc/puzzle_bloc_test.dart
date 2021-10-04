ecca2e766af420d7a5134eafb13e71fa0fb1712d
Allison Ryan <allisonryan0002@gmail.com>
2021-10-04 11:51:08 -0500

    test: add tests for puzzle bloc

---
+++ test/puzzle/bloc/puzzle_bloc_test.dart
@@ -0,0 +1,183 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:very_good_slide_puzzle/models/models.dart';
+import 'package:very_good_slide_puzzle/puzzle/puzzle.dart';
+
+void main() {
+  group('PuzzleBloc', () {
+    test('initial state is PuzzleState', () {
+      expect(
+        PuzzleBloc(4).state,
+        PuzzleState(),
+      );
+    });
+
+    group('Initialize', () {
+      const size = 1;
+      final position = Position(x: size, y: size);
+      final tile = Tile(
+        value: 0,
+        correctPosition: position,
+        currentPosition: position,
+      );
+
+      blocTest<PuzzleBloc, PuzzleState>(
+        'emits 1 tile when initialized with size 1',
+        build: () => PuzzleBloc(size),
+        act: (bloc) => bloc.add(Initialize()),
+        expect: () => <PuzzleState>[
+          PuzzleState(tiles: [tile])
+        ],
+      );
+    });
+    group('TileTapped', () {
+      const size = 3;
+      final topLeft = Position(x: 1, y: 1);
+      final topCenter = Position(x: 2, y: 1);
+      final topRight = Position(x: 3, y: 1);
+      final middleLeft = Position(x: 1, y: 2);
+      final middleCenter = Position(x: 2, y: 2);
+      final middleRight = Position(x: 3, y: 2);
+      final bottomLeft = Position(x: 1, y: 3);
+      final bottomCenter = Position(x: 2, y: 3);
+      final bottomRight = Position(x: 3, y: 3);
+
+      final topLeftTile = Tile(
+        value: 2,
+        correctPosition: topCenter,
+        currentPosition: topLeft,
+      );
+      final topCenterTile = Tile(
+        value: 1,
+        correctPosition: topLeft,
+        currentPosition: topCenter,
+      );
+      final topRightTile = Tile(
+        value: 3,
+        correctPosition: topRight,
+        currentPosition: topRight,
+      );
+      final middleLeftTile = Tile(
+        value: 4,
+        correctPosition: middleLeft,
+        currentPosition: middleLeft,
+      );
+      final middleCenterTile = Tile(
+        value: 5,
+        correctPosition: middleCenter,
+        currentPosition: middleCenter,
+      );
+      final middleRightTile = Tile(
+        value: 6,
+        correctPosition: middleRight,
+        currentPosition: middleRight,
+      );
+      final bottomLeftTile = Tile(
+        value: 7,
+        correctPosition: bottomLeft,
+        currentPosition: bottomLeft,
+      );
+      final bottomCenterTile = Tile(
+        value: 8,
+        correctPosition: bottomCenter,
+        currentPosition: bottomCenter,
+      );
+      final whitespaceTile = Tile(
+        value: 0,
+        correctPosition: bottomRight,
+        currentPosition: bottomRight,
+      );
+      final tiles = [
+        topLeftTile,
+        topCenterTile,
+        topRightTile,
+        middleLeftTile,
+        middleCenterTile,
+        middleRightTile,
+        bottomLeftTile,
+        bottomCenterTile,
+        whitespaceTile,
+      ];
+
+      blocTest<PuzzleBloc, PuzzleState>(
+        'emits [moved] when one tile was able to move',
+        build: () => PuzzleBloc(size),
+        seed: () => PuzzleState(tiles: tiles),
+        act: (bloc) => bloc.add(TileTapped(middleRightTile)),
+        expect: () => <PuzzleState>[
+          PuzzleState(
+            tiles: [
+              topLeftTile,
+              topCenterTile,
+              topRightTile,
+              middleLeftTile,
+              middleCenterTile,
+              Tile(
+                value: 6,
+                correctPosition: middleRight,
+                currentPosition: bottomRight,
+              ),
+              bottomLeftTile,
+              bottomCenterTile,
+              Tile(
+                value: 0,
+                correctPosition: bottomRight,
+                currentPosition: middleRight,
+              ),
+            ],
+            tileMovementStatus: TileMovementStatus.moved,
+          ),
+        ],
+      );
+
+      blocTest<PuzzleBloc, PuzzleState>(
+        'emits [moved] when mutiple tiles were able to move',
+        build: () => PuzzleBloc(size),
+        seed: () => PuzzleState(tiles: tiles),
+        act: (bloc) => bloc.add(TileTapped(topRightTile)),
+        expect: () => <PuzzleState>[
+          PuzzleState(
+            tiles: [
+              topLeftTile,
+              topCenterTile,
+              Tile(
+                value: 3,
+                correctPosition: topRight,
+                currentPosition: middleRight,
+              ),
+              middleLeftTile,
+              middleCenterTile,
+              Tile(
+                value: 6,
+                correctPosition: middleRight,
+                currentPosition: bottomRight,
+              ),
+              bottomLeftTile,
+              bottomCenterTile,
+              Tile(
+                value: 0,
+                correctPosition: bottomRight,
+                currentPosition: topRight,
+              ),
+            ],
+            tileMovementStatus: TileMovementStatus.moved,
+          ),
+        ],
+      );
+
+      blocTest<PuzzleBloc, PuzzleState>(
+        'emits [cannotBeMoved] when tapped tile cannot move to whitespace',
+        build: () => PuzzleBloc(size),
+        seed: () => PuzzleState(tiles: tiles),
+        act: (bloc) => bloc.add(TileTapped(topLeftTile)),
+        expect: () => <PuzzleState>[
+          PuzzleState(
+            tiles: tiles,
+            tileMovementStatus: TileMovementStatus.cannotBeMoved,
+          ),
+        ],
+      );
+    });
+  });
+}
