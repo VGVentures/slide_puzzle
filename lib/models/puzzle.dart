4e8acfa4795f544e9a1ee51f40f59d74fce9b77c
Allison Ryan <allisonryan0002@gmail.com>
2021-10-06 13:09:29 -0500

    refactor: move private logic methods to puzzle

--- lib/models/puzzle.dart
+++ lib/models/puzzle.dart
@@ -20,6 +20,27 @@ class Puzzle extends Equatable {
     return sqrt(tiles.length).toInt();
   }
 
+  /// Get the single whitespace tile object in the puzzle.
+  Tile getWhitespaceTile() {
+    return tiles.singleWhere((tile) => tile.value == 0);
+  }
+
+  /// Determines if the tapped tile can move in the direction of the whitespace
+  /// tile.
+  bool isTileMovable(Tile tile) {
+    final whitespaceTile = getWhitespaceTile();
+    if (tile == whitespaceTile) {
+      return false;
+    }
+
+    // A tile must be in the same row or column as the whitespace to move.
+    if (whitespaceTile.currentPosition.x != tile.currentPosition.x &&
+        whitespaceTile.currentPosition.y != tile.currentPosition.y) {
+      return false;
+    }
+    return true;
+  }
+
   /// Determines if the puzzle is solvable.
   bool isSolvable() {
     final size = getDimension();
@@ -73,6 +94,53 @@ class Puzzle extends Equatable {
       }
     }
     return false;
+  }
+
+  /// Shifts one or many tiles in a row/column with the whitespace and returns
+  /// the modified puzzle.
+  ///
+  // Recursively stores a list of all tiles that need to be moved and passes the
+  // list to _swapTiles to individually swap them.
+  Puzzle moveTiles(Tile tile, List<Tile> tilesToSwap) {
+    final whitespaceTile = getWhitespaceTile();
+    final deltaX = whitespaceTile.currentPosition.x - tile.currentPosition.x;
+    final deltaY = whitespaceTile.currentPosition.y - tile.currentPosition.y;
+
+    if ((deltaX.abs() + deltaY.abs()) > 1) {
+      final shiftPointX = tile.currentPosition.x + deltaX.sign;
+      final shiftPointY = tile.currentPosition.y + deltaY.sign;
+      final tileToSwapWith = tiles.singleWhere(
+        (tile) =>
+            tile.currentPosition.x == shiftPointX &&
+            tile.currentPosition.y == shiftPointY,
+      );
+      tilesToSwap.add(tile);
+      return moveTiles(tileToSwapWith, tilesToSwap);
+    } else {
+      tilesToSwap.add(tile);
+      return swapTiles(tilesToSwap);
+    }
+  }
+
+  /// Returns puzzle with new tile arrangement after individually swapping each
+  /// tile in tilesToSwap with the whitespace.
+  Puzzle swapTiles(List<Tile> tilesToSwap) {
+    for (final tileToSwap in tilesToSwap.reversed) {
+      final tileIndex = tiles.indexOf(tileToSwap);
+      final tile = tiles[tileIndex];
+      final whitespaceTile = getWhitespaceTile();
+      final whitespaceTileIndex = tiles.indexOf(whitespaceTile);
+
+      // Swap current board positions of the moving tile and the whitespace.
+      tiles[tileIndex] = tile.copyWith(
+        currentPosition: whitespaceTile.currentPosition,
+      );
+      tiles[whitespaceTileIndex] = whitespaceTile.copyWith(
+        currentPosition: tile.currentPosition,
+      );
+    }
+
+    return Puzzle(tiles: tiles);
   }
 
   @override
