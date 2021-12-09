5bdc3f7b53f5eb97692c958cbe01269b40740a10
Bartosz Selwesiuk <bartosz@selwesiuk.dev>
2021-12-09 12:15:49 +0100

    feat: update layout breakpoints (#26)

--- lib/layout/breakpoints.dart
+++ lib/layout/breakpoints.dart
@@ -1,10 +1,10 @@
 /// Defines the breakpoints for the puzzle UI.
 abstract class PuzzleBreakpoints {
   /// Max width for a small layout.
-  static const double small = 375;
+  static const double small = 576;
 
   /// Max width for a medium layout.
-  static const double medium = 768;
+  static const double medium = 1200;
 
   /// Max width for a large layout.
   static const double large = 1440;
