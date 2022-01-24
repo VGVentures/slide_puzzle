bec6ece74b3819819dc65670468b30c96b9724b0
Chris Fulton <828498+angwonson@users.noreply.github.com>
2022-01-24 15:51:46 -0800

    reran builder on repository models

--- packages/opensea_repository/lib/src/models/artwork.g.dart
+++ packages/opensea_repository/lib/src/models/artwork.g.dart
@@ -15,15 +15,12 @@ Artwork _$ArtworkFromJson(Map<String, dynamic> json) {
       name: $checkedConvert(json, 'name', (v) => v as String),
       description: $checkedConvert(json, 'description', (v) => v as String),
       creator: $checkedConvert(json, 'creator', (v) => v as String),
-      creatorProfileUrl:
-          $checkedConvert(json, 'creator_profile_url', (v) => v as String),
       creatorProfileImage:
           $checkedConvert(json, 'creator_profile_image', (v) => v as String),
     );
     return val;
   }, fieldKeyMap: const {
     'imageUrl': 'image_url',
-    'creatorProfileUrl': 'creator_profile_url',
     'creatorProfileImage': 'creator_profile_image'
   });
 }
@@ -35,6 +32,5 @@ Map<String, dynamic> _$ArtworkToJson(Artwork instance) => <String, dynamic>{
       'name': instance.name,
       'description': instance.description,
       'creator': instance.creator,
-      'creator_profile_url': instance.creatorProfileUrl,
       'creator_profile_image': instance.creatorProfileImage,
     };
