8f7b6b51c6762ea92a252a5458658ab3e243fdaf
Prabowo Murti <prabowo.murti@gmail.com>
2022-03-13 23:50:59 +0700

    feat: modify shuffling algorithm on RotadeTheme

--- lib/puzzle/bloc/puzzle_bloc.dart
+++ lib/puzzle/bloc/puzzle_bloc.dart
@@ -4,13 +4,18 @@ import 'dart:math';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 import 'package:very_good_slide_puzzle/models/models.dart';
+import 'package:very_good_slide_puzzle/rotade/rotade.dart';
+import 'package:very_good_slide_puzzle/simple/simple_theme.dart';
+import 'package:very_good_slide_puzzle/theme/themes/puzzle_theme.dart';
 
 part 'puzzle_event.dart';
 part 'puzzle_state.dart';
 
 class PuzzleBloc extends Bloc<PuzzleEvent, PuzzleState> {
-  PuzzleBloc(this._size, {this.random}) : super(const PuzzleState()) {
+  PuzzleBloc(this._size, {this.random, this.theme})
+      : super(const PuzzleState()) {
     on<PuzzleInitialized>(_onPuzzleInitialized);
     on<TileTapped>(_onTileTapped);
     on<PuzzleReset>(_onPuzzleReset);
@@ -18,6 +23,8 @@ class PuzzleBloc extends Bloc<PuzzleEvent, PuzzleState> {
   }
 
   final int _size;
+
+  final PuzzleTheme? theme;
 
   final Random? random;
 
@@ -136,7 +143,10 @@ class PuzzleBloc extends Bloc<PuzzleEvent, PuzzleState> {
       }
     }
 
-    if (shuffle) {
+    // TODO(prabowomurti): another workaround to determine current theme
+    final isRotadeTheme = state.lastTappedTile == null;
+
+    if (shuffle && !isRotadeTheme) {
       // Randomize only the current tile posistions.
       currentPositions.shuffle(random);
     }
@@ -150,20 +160,104 @@ class PuzzleBloc extends Bloc<PuzzleEvent, PuzzleState> {
     var puzzle = Puzzle(tiles: tiles);
 
     if (shuffle) {
-      // Assign the tiles new current positions until the puzzle is solvable and
-      // zero tiles are in their correct position.
-      while (!puzzle.isSolvable() || puzzle.getNumberOfCorrectTiles() != 0) {
-        currentPositions.shuffle(random);
-        tiles = _getTileListFromPositions(
-          size,
-          correctPositions,
-          currentPositions,
-        );
-        puzzle = Puzzle(tiles: tiles);
+      if (isRotadeTheme) {
+        var rotadeTilePositions = currentPositions;
+        while (puzzle.getNumberOfCorrectTiles() != 0) {
+          rotadeTilePositions = _randomizeTiles(rotadeTilePositions, size);
+          tiles = _getTileListFromPositions(
+            size,
+            correctPositions,
+            rotadeTilePositions,
+          );
+          puzzle = Puzzle(tiles: tiles);
+        }
+      } else {
+        // Assign the tiles new current positions until the puzzle is solvable and
+        // zero tiles are in their correct position.
+        while (!puzzle.isSolvable() || puzzle.getNumberOfCorrectTiles() != 0) {
+          currentPositions.shuffle(random);
+          tiles = _getTileListFromPositions(
+            size,
+            correctPositions,
+            currentPositions,
+          );
+          puzzle = Puzzle(tiles: tiles);
+        }
       }
     }
 
     return puzzle;
+  }
+
+  /// Simulating rotate tiles for the puzzle
+  List<Position> _randomizeTiles(List<Position> _positions, int size) {
+    final rotadeSize = _getNumberOfRotadeElements(size);
+
+    // generate list of rotade btn index
+    final rotades = List<int>.generate(rotadeSize, (i) => i);
+    final randomRotadeIndex = (rotades..shuffle()).first;
+    final topLeftIndex = (randomRotadeIndex ~/ (size - 1)) * size +
+        (randomRotadeIndex % (size - 1));
+
+    return _rotateTiles(_positions, topLeftIndex, size);
+  }
+
+  /// Get number of rotade elements
+  int _getNumberOfRotadeElements(int size) {
+    return (size - 1) * (size - 1);
+  }
+
+  /// Rotate 4 tiles start from [topLeftIndex], randomly
+  //   ┌──────┐ ┌──────┐
+  //   │ tli  │ │ tli+1|
+  //   └──────┘ └──────┘
+  //      [rotadeBtn]
+  //   ┌──────┐ ┌──────┐
+  //   │ tl+s │ │tl+s+1│
+  //   └──────┘ └──────┘
+  List<Position> _rotateTiles(
+    List<Position> pos,
+    int topLeftIndex,
+    int tileSize,
+  ) {
+    // random rotation
+    final randomRotate = ([90, 180, 270]..shuffle()).first;
+
+    final topLeft = topLeftIndex;
+    final topRight = topLeft + 1;
+    final bottomLeft = topLeft + tileSize;
+    final bottomRight = bottomLeft + 1;
+
+    switch (randomRotate) {
+      case 90:
+        final temp = pos[topRight];
+        pos[topRight] = pos[bottomRight];
+        pos[bottomRight] = pos[bottomLeft];
+        pos[bottomLeft] = pos[topLeft];
+        pos[topLeft] = temp;
+        break;
+
+      case 180:
+        final temp1 = pos[topLeft];
+        pos[topLeft] = pos[bottomRight];
+        pos[bottomRight] = temp1;
+
+        final temp2 = pos[topRight];
+        pos[topRight] = pos[bottomLeft];
+        pos[bottomLeft] = temp2;
+        break;
+
+      case 270: // equals to -90
+        final temp = pos[bottomLeft];
+        pos[bottomLeft] = pos[bottomRight];
+        pos[bottomRight] = pos[topRight];
+        pos[topRight] = pos[topLeft];
+        pos[topLeft] = temp;
+
+        break;
+    }
+
+    return pos;
   }
 
   /// Build a list of tiles - giving each tile their correct position and a
